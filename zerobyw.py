bea3ee0340a541119fd3c7cb3aaf86d3d1253ae2
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2023-04-21 15:09:06 +0800

    Check Zerobyw domain

--- zerobyw.py
+++ zerobyw.py
@@ -5,7 +5,12 @@ try:
     if res.status_code != 200:
         raise Exception(f'HTTP {res.status_code}')
 
-    url = res.json()['url']
+    url = res.json()['url'].strip().rstrip('/')
+
+    scheme, sep, domain = url.partition('://')
+    assert sep == '://'
+    assert scheme == 'http' or scheme == 'https'
+    assert '/' not in domain
 
     with open('zerobyw.txt', 'w') as f:
         f.write(url)
