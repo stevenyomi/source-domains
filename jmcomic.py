c050e63eac8e9586520b6895f7109be4451ff3c7
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2024-08-16 17:24:22 +0000

    Move JM link domain to data branch

--- jmcomic.py
+++ jmcomic.py
@@ -1,11 +1,26 @@
 import re
 
+from httpx import URL, Client, Response
+
 from common import http_get, write_result
 
-def main() -> None:
-    res = http_get('https://jmcomicgo-xyz.translate.goog/?_x_tr_sl=auto&_x_tr_tl=zh-TW', headers={
+def get_domain(client: Client, domain: str) -> Response:
+    return client.get(f'https://{domain.replace('-', '--').replace('.', '-')}.translate.goog/?_x_tr_sl=auto&_x_tr_tl=zh-TW', headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     })
+
+
+def main() -> None:
+    with open('jmcomic-link.txt') as f:
+        link_domain = f.read()
+
+    with Client() as client:
+        if (res := get_domain(client, link_domain)).is_redirect:
+            subdomain, _, domain = URL(res.headers['Location']).host.partition('.')
+            assert domain == 'translate.goog'
+            link_domain = subdomain.replace('--', '#').replace('-', '.').replace('#', '-')
+            write_result('jmcomic-link.txt', link_domain, f'JM-link {link_domain}')
+            assert (res := get_domain(client, link_domain)).is_success
 
     text = (text := res.text)[(index := text.index('<body>') + 6) : text.index('<script>', index)]
     text = text.replace('&nbsp;', ' ')
