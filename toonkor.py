fd21a041bfda06c1dcf579e380493285359113cf
stevenyomi <95685115+stevenyomi@users.noreply.github.com>
2024-09-18 03:28:55 +0000

    Catch IDNA error

--- toonkor.py
+++ toonkor.py
@@ -1,11 +1,22 @@
 import re
 
+import idna
 from httpx import Client
 
 from common import extract_location, get_domain, write_result
 
 
 def main() -> None:
+    decode = idna.decode
+
+    def wrapped_decode(*args, **kwargs):
+        try:
+            return decode(*args, **kwargs)
+        except Exception:
+            return args[0]
+
+    idna.decode = decode
+
     with Client() as client:
         response = get_domain(client, "xn--yq5bv6mzmcca.org")
         assert response.is_redirect
